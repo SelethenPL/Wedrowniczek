62d49933dbab45a74449035b912259f19d5dd88c
Selethen <lukisd@op.pl>
2019-06-04 00:27:19 +0200

    Klasa testowa odpowedzialna za nic.

---
+++ app/src/main/java/TestClass.java
@@ -0,0 +1,18 @@
+public class TestClass {
+
+    private int x;
+    private int y;
+
+    TestClass(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getx() {
+        return x;
+    }
+    public int gety() {
+        return y;
+    }
+
+}
