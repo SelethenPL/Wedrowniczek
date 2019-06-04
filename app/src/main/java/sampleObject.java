670510775fa86a763b361fd5650eddf748e842ad
Ewa98 <komcia1234@gmail.com>
2019-06-04 16:49:50 +0200

    View Activity - version 16:50

---
+++ app/src/main/java/sampleObject.java
@@ -0,0 +1,30 @@
+import com.example.wedrowniczek.R;
+import com.example.wedrowniczek.View_Activity;
+
+public class sampleObject {
+
+    View_Activity view_activity = new View_Activity("Święty Marcin 80/82\n 61-717 Poznań",
+                "wtorek\t12:00–20:00\n" +
+                "środa\t12:00–20:00\n" +
+                "czwartek\t12:00–20:00\n" +
+                "piątek\t12:00–20:00\n" +
+                "sobota\t12:00–20:00\n" +
+                "niedziela\n" +
+                "12:00–20:00",
+            "4.8",
+            "Centrum Kultury ZAMEK w Poznaniu to jedna z największych instytucji kultury w kraju. " +
+                  "Każdego roku odbywa się w nim 2500 wydarzeń, które prezentują i stymulują rozwój najciekawszych zjawisk kultury. " +
+                  "W przestrzeniach ZAMKU przenikają się i wzajemnie dopełniają sztuki wizualne, teatr, film, muzyka i literatura.",
+             R.drawable.zamek);
+}
+
+    /*View_Activity view_activity = new View_Activity("ul. Wodna 27\n61-781 Poznań", "poniedziałek\tZamknięte\n" +
+            "wtorek\t09:00–16:00\n" +
+            "środa\t09:00–16:00\n" +
+            "czwartek\t09:00–16:00\n" +
+            "piąte\t10:00–17:00\n" +
+            "sobota\t10:00–17:00\n" +
+            "niedziela\t12:00–16:00", "4.5", "Posiada cenne zbiory archeologiczne z terenów całej Polski oraz kolekcje pozaeuropejskie."+
+            "Placówka udostępnia zwiedzającym wystawy stałe oraz czasowe. " +
+            "Zajmuje się także działalnością edukacyjną, ochroną zabytków oraz ochroną dziedzictwa archeologicznego. " +
+            "Obecna siedziba muzeum mieści się w Pałacu Górków przy ulicy Wodnej w Poznaniu.", R.drawable.muzeum_archeologiczne);*/
