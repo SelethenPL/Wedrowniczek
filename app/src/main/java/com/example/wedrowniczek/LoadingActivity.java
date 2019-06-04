0a671835115b20b4460f9c8ee78721ac06975a68
Krymeq <krymeq353@gmail.com>
2019-06-04 14:28:57 +0200

    Updated some values in code

--- app/src/main/java/com/example/wedrowniczek/LoadingActivity.java
+++ app/src/main/java/com/example/wedrowniczek/LoadingActivity.java
@@ -22,11 +22,11 @@ public class LoadingActivity extends AppCompatActivity {
 
     public void load()
     {
-        for(int i = 0; i < (new Random()).nextInt(25) + 2; i++)
+        for(int i = 0; i < (new Random()).nextInt(25) + 20; i++)
         {
             try
             {
-                Thread.sleep(400);
+                Thread.sleep(100);
             }
             catch (InterruptedException ex)
             {
