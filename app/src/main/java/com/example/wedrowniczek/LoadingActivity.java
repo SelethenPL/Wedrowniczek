9612d5382b864ce348e63a2c0f64436d60a50754
Krymeq <krymeq353@gmail.com>
2019-06-04 14:00:07 +0200

    Finished loading activity with sample switch to main activity

--- app/src/main/java/com/example/wedrowniczek/LoadingActivity.java
+++ app/src/main/java/com/example/wedrowniczek/LoadingActivity.java
@@ -1,11 +1,11 @@
 package com.example.wedrowniczek;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+
+import java.util.Random;
 
 public class LoadingActivity extends AppCompatActivity {
 
@@ -15,5 +15,29 @@ public class LoadingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_loading);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Thread t = new Thread(() ->  load());
+        t.start();
     }
+
+    public void load()
+    {
+        for(int i = 0; i < (new Random()).nextInt(25) + 2; i++)
+        {
+            try
+            {
+                Thread.sleep(400);
+            }
+            catch (InterruptedException ex)
+            {
+
+            }
+        }
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        this.finish();
+    }
+
+
 }
